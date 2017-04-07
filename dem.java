730fb12918a80876af7cbc4fdc26c3b16afbd92c
amirtharavanan <amirtha.ravanan@gmail.com>
2017-04-07 17:12:30 +0530

    Create dem.java

---
+++ dem.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class dem
+{
+public static void main(String args[])
+{
+int num;
+System.out.println("enter the value of num",+num);
+if(num%2==0)
+{
+System.out.println("the number is even");
+}
+else
+{
+System.out.println("the number is odd");
+}
+}
+}
