cdeaf9e653774c34648e1b05470c4f02608e04dc
amirtharavanan <amirtha.ravanan@gmail.com>
2017-04-07 17:19:20 +0530

    Update and rename dem.java to dem.c

---
+++ dem.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int num;
+  printf(enter the value of num");
+  scanf("%d",&num);
+         if(num%2==0)
+         {
+           printf("the number is even");
+         }
+         else
+         {
+         printf("the number is odd");
+         }
+  }
+         
